56e1491e350f99c311bb6fbdd6a159a171baf9b1
AlteaF <98816319+AlteaF@users.noreply.github.com>
2026-03-30 10:27:03 +0200

    welcome page and some cooler changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ st.markdown("""
     
     /* 2. Added margin-top to ensure it's below the top bar */
     .counter-box { 
-        background-color: #00F2E8; 
+        background-color: #FFFFFF; 
         padding: 12px; 
         border-radius: 8px; 
         border-left: 6px solid #FF5C00;
@@ -90,14 +90,13 @@ if "points" not in st.session_state:
 if not st.session_state.session_started:
     st.markdown("<br>", unsafe_allow_html=True)
     st.header("🦪 Mussel Annotation Project", divider="rainbow")
-    st.markdown("Hello and welcome to my annotation app. \n Thank you for participating and helping with my Master Thesis!!", text_alignment = "center")
-    st.markdown("""This project is focused on assessing annotation perfromance on **Blue Mussels** images collected in the Limfjord, in Northern Jutland.\n 
-    By annotating the images that will be presented, you will help me in the analysis of the data, which will be used for my Master Thesis. \n
-    The analysis will focus on mussel counting and time of annotation, thus the time will be recorded. Please take as much time as you need. \n
-    A "Break" function will be provided, such that the time will be stopped when needed. \n
-
-    Below, you can find a field in which to write your name. The name will be used to save your annotations, and to collect previous annotations, if you haven't finished in a previous session, or need to correct something. 
-    You will be asked if you want to start a new session or continue with the previous. """)
+    st.markdown("Hello and welcome to my annotation app. \n Thank you for participating and helping with my Master Thesis!", text_alignment = "center")
+    st.markdown("This project is focused on assessing annotation perfromance on **Blue Mussels** images collected in the Limfjord, in Northern Jutland.")
+    st.markdown("By annotating the images that will be presented, you will help me in the analysis of the data, which will be used for my Master Thesis.")
+    st.markdown("The analysis will focus on mussel counting and time of annotation, thus the time will be recorded. Please take as much time as you need.")
+    st.markdown("A \"Break\" function will be provided, such that the time will be stopped when needed.")
+    st.markdown("Below, you can find a field in which to write your name. The name will be used to save your annotations, and to collect previous annotations, if you haven't finished in a previous session, or need to correct something.") 
+    st.markdown("You will be asked if you want to start a new session or continue with the previous.")
     name_input = st.text_input("Enter your name:").strip()
     if name_input:
         res = github_request("GET", "")
